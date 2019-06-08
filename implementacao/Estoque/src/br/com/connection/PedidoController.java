ddcae35b8fd8cf6fdf1136901352dfff3a7ad35d
robertotics4 <robertooliveiragp@gmail.com>
2019-06-07 23:42:09 -0300

    Criando o controle de pedidos

--- implementacao/Estoque/src/br/com/connection/PedidoController.java
+++ implementacao/Estoque/src/br/com/connection/PedidoController.java
@@ -5,17 +5,31 @@
  */
 package br.com.connection;
 
+import br.com.dao.PedidoEstoqueDAO;
+import static br.com.model.Filial_.estoque;
 import br.com.model.PedidoEstoque;
 
 /**
+ * classe de controle dos pedidos
  *
  * @author Roberto Oliveira
  */
 public abstract class PedidoController {
 
-    public static void confirmOlder (PedidoEstoque pedidoEstoque) {
-        pedidoEstoque.getItensPedido().stream().forEach(ip -> {
-            
-        });
+    public static boolean confirmOlder(PedidoEstoque pedidoEstoque) {
+        PedidoEstoqueDAO pedao = new PedidoEstoqueDAO();
+        Estoque estoque = pedidoEstoque.getFilial().getEstoque();
+
+        if (pedidoEstoque.getTipoPedido() == PedidoEstoque.TipoPedido.SAIDA) {
+            pedidoEstoque.getItensPedido().stream().forEach(ip -> {
+                if (estoque.isAvailable(ip.getProduto(), ip.getQuantidade())) {
+
+                } else {
+
+                }
+            });
+        }
+
+        return false;
     }
 }
